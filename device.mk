5a0712be87ad6c654dcdcf4040531df9ddc005ae
UtsavisGreat <utsavbalar1231@gmail.com>
2019-06-27 14:01:50 +0000

    c106: hw

--- device.mk
+++ device.mk
@@ -24,7 +24,7 @@ $(call inherit-product, vendor/coolpad/c106/c106-vendor.mk)
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
